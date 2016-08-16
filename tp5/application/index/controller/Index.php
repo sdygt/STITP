9f501f234240d7d112c6001497bac71f2353967c
sdygt <sdygt@users.noreply.github.com>
2016-08-16 16:11:13 +0800

    Deploy ThinkPHP 5.0 framework

---
+++ tp5/application/index/controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\index\controller;
+
+use think\Controller;
+use think\View;
+class Index extends Controller
+{
+    public function index()
+    {
+        $view = new View();
+        return $view->fetch();
+    }
+}

e98985cae71bc01f77d1af81855a4687574422a3
sdygt <sdygt@users.noreply.github.com>
2016-10-04 22:36:58 +0800

    PHP: intetact with shell

---
+++ tp5/application/api/controller/Phpapi.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Richard
+ * Date: 2016/10/2
+ * Time: 21:13
+ */
+
+namespace app\api\controller;
+
+
+use think\Controller;
+
+
+class phpapi extends Controller
+{
+    public function CheckSyntax()
+    {
+        $file = request()->file('file-0');
+
+        if ($file) {
+            $info = $file->rule(function ($file) {
+                return substr($file->hash('md5'), 0, 8) . '_' . preg_replace('/\h+/', '_', $file->getInfo('name'));
+            })->move(ROOT_PATH . 'runtime' . DS . 'uploads');
+            $pathname = $info->getPathname();
+        } else {
+            return ['status' => false];
+        }
+
+        ob_start();
+        passthru('clang -fsyntax-only ' . '"' . $pathname . '"' . ' 2>&1', $ret);
+        $stdout = ob_get_clean();
+        if ($ret === 0) {
+            return ['status' => true, 'message' => 'Syntax check successful'];
+        } else {
+            return ['status' => true, 'message' => $stdout];
+        }
+    }
+
+    public function LLVMSlice()
+    {
+        $arg_m = input('post.arg_m');
+        $arg_d = input('post.arg_d');
+
+        if ( !in_array($arg_d, ['Bwd', 'Fwd', 'Both'])) {
+            $arg_d = 'Bwd';
+        }
+        if ( !in_array($arg_m, ['Symbolic', 'Weiser', 'SDG', 'IFDS'])) {
+            $arg_m = 'Symbolic';
+        }
+
+        $file = request()->file('file-0');
+        if ($file) {
+            $info = $file->rule(function ($file) {
+                return substr($file->hash('md5'), 0, 8) . '_' . preg_replace('/\h+/', '_', $file->getInfo('name'));
+            })->move(ROOT_PATH . 'runtime' . DS . 'uploads');
+            $pathname = $info->getPathname();
+        } else {
+            return ['status' => false];
+        }
+        $cmdline = "llvm-slicing -m {$arg_m} -d {$arg_d} {$pathname} 2>&1";
+        ob_start();
+        echo time();
+        echo "{$arg_m} {$arg_d}" . PHP_EOL;
+
+        passthru($cmdline, $ret);
+        $stdout = ob_get_clean();
+        $this->result($stdout, $ret, 'success');
+
+    }
+}
